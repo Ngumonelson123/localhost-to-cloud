b199fe5b9884c7192e814aff99d7c2763c1d8c28
Nelson Ngumo <nelsonmbui88@gmail.com>
2026-03-14 05:33:05 +0300

    fix: disable static index so / returns JSON not HTML

--- app/index.js
+++ app/index.js
@@ -5,7 +5,7 @@ const path = require('path');
 
 const app = express();
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'), { index: false }));
 
 const pool = new Pool({
   host:     process.env.DB_HOST,
